4b0381f7551b46c14820d93d92c483c6762ca4b0
danielpalomo <danielpalomo@localhost>
2025-04-02 14:14:54 -0500

    Studio 8

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -3,6 +3,10 @@ package studio8;
 import support.cse131.NotYetImplementedException;
 
 public class MultipleChoiceQuestion extends Question {
+	private String prompt;
+	private String answer;
+	private int points;
+	private String[] choices;
 
 	/**
 	 * Constructor
@@ -12,18 +16,21 @@ public class MultipleChoiceQuestion extends Question {
 	 * @param choices
 	 */
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
-		// Call the super class constructor, then create and set
-		// instance variables for any values that aren't handled
-		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
 	
 	/**
 	 * Display the prompt for the question in addition to 
 	 * the choices present for the question.
 	 */
+	@Override
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		super.displayPrompt();
+		
+	for (int i = 0; i < choices.length; i++) {	
+		System.out.println((i + 1) + ". " + choices[i]);
+	}
 	}
 	
 	/**
@@ -31,11 +38,13 @@ public class MultipleChoiceQuestion extends Question {
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return choices;
 	}
 	
+
 	public static void main(String[] args) {
-		// TODO: create your own MultipleChoiceQuestion
+		String[] choicesone = {"A", "B", "C", "D"};
+		MultipleChoiceQuestion one = new MultipleChoiceQuestion("What is the answer?", "B", 5, choicesone);
 	}
 
 }
--- src/studio8/Question.java
+++ src/studio8/Question.java
@@ -3,6 +3,9 @@ package studio8;
 import support.cse131.NotYetImplementedException;
 
 public class Question {
+private String prompt;
+private String answer;
+private int points;
 	
 	/**
 	 * Constructor
@@ -11,7 +14,9 @@ public class Question {
 	 * @param points
 	 */
 	public Question(String prompt, String answer, int points) {
-		throw new NotYetImplementedException();
+		this.prompt = prompt;
+		this.answer = answer;
+		this.points = points;
 	}
 	
 	/**
@@ -40,7 +45,7 @@ public class Question {
 	 * @return int points
 	 */
 	public int getPoints() {
-		throw new NotYetImplementedException();
+		return points;
 	}
 	
 	/**
@@ -48,10 +53,11 @@ public class Question {
 	 * @return String answer
 	 */
 	public String getAnswer() {
-		throw new NotYetImplementedException();
+		return answer;
 	}
 	
 	public static void main(String[] args) {
-		// TODO: Create a Question object of your own!
+		Question one = new Question("What is my favorite food?", "Tacos", 5);
+		one.displayPrompt();
 	}
 }
